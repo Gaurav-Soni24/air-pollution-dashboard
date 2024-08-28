66e85123560e873b1b1ea13987cd572392b49e21
Gaurav-Soni24 <sonigaurav2021@gmail.com>
2024-08-28 13:35:44 +0530

    Initial commit from Create Next App

---
+++ Components/Body.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Navbar from './NavBar/Navbar'
+import AqiCard from './AQI/AqiCard';
+import './Body.css'
+import HistoricalAqiGraph from './HistoryCard/HistoricalAqiGraph'
+import MapCard from './Map/MapCard';
+const Body = () => {
+
+  const [log, setLog] = useState(null);
+  const [lat, setLat] = useState(null);
+
+  return (
+    <div className='body'>
+      <Navbar setLat={setLat} setLog={setLog} />
+      <div className="dashboard">
+        <AqiCard lat={lat} log={log} />
+        <MapCard lat={lat} log={log} setLog={setLog} setLat={setLat}/>
+        <HistoricalAqiGraph lat={lat} log={log} />
+      </div>
+    </div>
+  )
+}
+
+export default Body
---
+++ Components/NavBar/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='logo'>
+      Air<span>Watch</span>
+    </div>
+  )
+}
+
+export default Logo
